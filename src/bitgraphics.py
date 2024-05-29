00ea53ca86c566d42f2c10cafd276d560bad0ec9
Tim Hanewich <tahanewich@live.com>
2024-05-29 15:34:45 -0400

    BitGraphic core

--- src/bitgraphics.py
+++ src/bitgraphics.py
@@ -7,12 +7,33 @@ class BitGraphic:
         self.height:int = 0
 
     def to_json(self) -> str:
-        ToReturn = {"bits": self.bits, "width": self.width, "height": self.height}
+
+        # assemble bits as 1's and 0's
+        bitsbin:str = ""
+        for bit in self.bits:
+            if bit:
+                bitsbin = bitsbin + "1"
+            else:
+                bitsbin = bitsbin + "0"
+
+        ToReturn = {"bits": bitsbin, "width": self.width, "height": self.height}
         return json.dumps(ToReturn)
     
+    def from_json(self, jsons:str) -> None:
 
-bg = BitGraphic()
-bg.bits = [False, True, False, True]
-bg.width = 200
-bg.height = 100
-print(bg.to_json())
+        obj = json.loads(jsons)
+
+        # deserialize bits
+        self.bits.clear()
+        for c in obj["bits"]:
+            if c == "0":
+                self.bits.append(False)
+            elif c == "1":
+                self.bits.append(True)
+            else:
+                raise Exception("Character '" + c + "' in JSON-serializes 'bits' property is not a valid 0 or 1.")
+        
+        # width and height
+        self.width = obj["width"]
+        self.height = obj["height"]
+            
